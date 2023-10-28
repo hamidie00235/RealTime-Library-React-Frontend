a3da08de8ca18f2481815098d0964cb205d192d7
hamidie00235 <ahmathisseinh@gmail.com>
2023-10-28 10:59:11 +0300

    React onClick Listeners

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import TodoTable from './components/TodoTable';
 
 function App() {
+
+   const todos=[
+  {rowNumber:1,rowDescription:'Feed puppy',rowAssigned:'User one'},
+  {rowNumber:2,rowDescription:'Water plants',rowAssigned:'User two'},
+  {rowNumber:3,rowDescription:'Make dinner',rowAssigned:'User two'},
+  {rowNumber:4,rowDescription:'Make dinner early',rowAssigned:'User two'}
+  ]
+ const addTodo =() =>{
+  if(todos.length>0){
+    const newTodo={rowNumber:todos.length+1,
+      rowDescription:'new todo',
+      rowAssigned:'User three'};
+      todos.push(newTodo);
+      console.log(newTodo);
+
+ }
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='mt-5 container'>
+      <div className='card'>
+     <div className='card-header'> Your Todo's
+      </div>   
+      <div className='card-body'> 
+  <TodoTable todos={todos}/> 
+  <button className='btn btn-primary' onClick={addTodo}>Add new todo</button>
+    
+   </div>
+   </div>
     </div>
   );
 }
---
+++ src/components/TodoRowItem.js
@@ -0,0 +1,13 @@
+function TodoRowItem(props){
+    return ( 
+    <tr>
+        <th scope='row'>{props.rowNumber}</th>
+        <td>{props.rowDescription}</td>
+        <td>{props.rowAssigned}</td>
+       
+      </tr>
+      )
+       
+    
+}
+export default TodoRowItem
