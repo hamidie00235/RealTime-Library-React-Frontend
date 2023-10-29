4bc66ab86119da2a6e59f55d94a397baecfce81b
hamidie00235 <ahmathisseinh@gmail.com>
2023-10-29 11:48:30 +0300

    Adding User Input to React Form

--- src/components/NewTodoForm.js
+++ src/components/NewTodoForm.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 function NewTodoForm(){
+    const [description,setDescription] =useState('');
+    const [assigned ,setAssigned] = useState('');
+
+    // const descriptionChange =(event) => {
+    //     console.log('description',event.target.value)
+    //     setDescription(event.target.value)
+
+    // }
+    // const assignedChange =(event) => {
+    //     console.log('assigned',event.target.value)
+    //     setAssigned(event.target.value)
+    // }
+
     return (
 
         <div className='mt-5'>
             <form>
                <div className='mb-3'>
                 <lab className='form-label'>Assigned</lab>
-                <input type="text" className='form-control' required></input>
+                <input
+                 type="text" 
+                 className='form-control'
+                  required
+                  onChange={e=>setAssigned(e.target.value)}
+                  value={assigned}
+                  ></input>
                 </div> 
                 <div className='mb-3'>
                     <label className='form-label'>Description</label>
-                    <textarea type='text' className='form-control' rows={3} required></textarea>
+                    <textarea 
+                    type='text' 
+                    className='form-control'
+                     rows={3} 
+                     required
+                     onChange={e=>setDescription(e.target.value)} 
+                     value={description}
+                     ></textarea>
 
                 </div>
                 <button type='button' className='btn btn-primary mt-3'>Add Todo</button>
